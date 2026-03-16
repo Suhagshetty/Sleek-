735fe24e04b0f15ca0ab5ec79098c9e18eef0874
Suhagshetty <suhagshetty07@gmail.com>
2026-03-16 21:42:49 +0530

    feat:- slug

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className="font-extrabold text-2xl text-center">Welcome to Sleek</h2>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
